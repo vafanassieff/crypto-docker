12474528f9b90ed49b63d618cc70e25420cd6d85
Victor Afanassieff <victor@afanassieff.com>
2020-03-12 16:31:16 +0100

    Added tor image and tor example

---
+++ tor/build.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Building tor"
+cd ./docker
+
+# For docker hub ...
+IMAGE_NAME="vafanassieff/tor"
+docker build -t $IMAGE_NAME .
+echo "$IMAGE_NAME built √"
+
+if [ "$1" == "registry" ]; then
+    docker push $IMAGE_NAME
+    echo "$IMAGE_NAME pushed √"
+    if [ "$2" == "latest" ]; then
+        echo "Add the latest tag to the image"
+        docker tag  $IMAGE_NAME $IMAGE_NAME:latest
+        docker push $IMAGE_NAME:latest
+        echo "$IMAGE_NAME:latest pushed √"
+    fi
+fi
+
+# For my personal registry ...
+IMAGE_NAME="registry.afa.ovh/tor"
+docker build -t $IMAGE_NAME .
+if [ "$1" == "registry" ]; then
+    docker push $IMAGE_NAME
+    echo "$IMAGE_NAME pushed √"
+    if [ "$2" == "latest" ]; then
+        echo "Add the latest tag to the image"
+        docker tag  $IMAGE_NAME $IMAGE_NAME:latest
+        docker push $IMAGE_NAME:latest
+        echo "$IMAGE_NAME:latest pushed √"
+    fi
+fi
