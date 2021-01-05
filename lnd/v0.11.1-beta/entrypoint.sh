cf9abcdd6b8c32d5e3aec2a1410fd75d1806508f
Victor Afanassieff <victor@afanassieff.com>
2021-01-05 23:02:55 +0100

    Better entrypoint

--- lnd/v0.11.1-beta/entrypoint.sh
+++ lnd/v0.11.1-beta/entrypoint.sh
@@ -35,7 +35,8 @@ if [ ! '$(stat -c %u "${LND_PATH}")' = "$(id -u lnd)" ]; then
   chown -R lnd:lnd $LND_PATH
 fi
 
-if [ "$1" = "lnd" ] || [ "$1" = "lnd-cli" ] || [ "$1" = "lnd-tx" ]; then
+if [ "$1" = "lnd" ]; then
+  if [ -f "$LND_CONF_PATH" ]; then
 
   edit_conf () {
     if grep -q "^$1=" $LND_CONF_PATH;then
@@ -48,33 +49,44 @@ if [ "$1" = "lnd" ] || [ "$1" = "lnd-cli" ] || [ "$1" = "lnd-tx" ]; then
     fi
   }
 
-  if ! grep -q "\[Bitcoind\]" $LND_CONF_PATH;then
-    echo -e "\n[Bitcoind]" >> $LND_CONF_PATH
-  fi
+    if ! grep -q "\[Bitcoind\]" $LND_CONF_PATH;then
+      echo -e "\n[Bitcoind]" >> $LND_CONF_PATH
+    fi
 
-  # Configuring lnd may add more option later ...
-  if [ ! -z "${BITCOIND_RPCUSER}" ] && [ -f "$LND_CONF_PATH" ]; then
-    edit_conf "bitcoind.rpcuser" ${BITCOIND_RPCUSER}
-  fi
+    # Configuring lnd may add more option later ...
+    if [ ! -z "${BITCOIND_RPCUSER}" ] && [ -f "$LND_CONF_PATH" ]; then
+      edit_conf "bitcoind.rpcuser" ${BITCOIND_RPCUSER}
+    fi
 
-  if [ ! -z "${BITCOIND_RPCPASS}" ] && [ -f "$LND_CONF_PATH" ]; then
-    edit_conf "bitcoind.rpcpass" ${BITCOIND_RPCPASS}
-  fi
+    if [ ! -z "${BITCOIND_RPCPASS}" ] && [ -f "$LND_CONF_PATH" ]; then
+      edit_conf "bitcoind.rpcpass" ${BITCOIND_RPCPASS}
+    fi
 
-  if [ ! -z "${BITCOIND_RPCHOST}" ] && [ -f "$LND_CONF_PATH" ]; then
-    edit_conf "bitcoind.rpchost" ${BITCOIND_RPCHOST}
-  fi
+    if [ ! -z "${BITCOIND_RPCHOST}" ] && [ -f "$LND_CONF_PATH" ]; then
+      edit_conf "bitcoind.rpchost" ${BITCOIND_RPCHOST}
+    fi
 
-  if [ ! -z "${BITCOIND_ZMQRAWBLOCK}" ] && [ -f "$LND_CONF_PATH" ]; then
-    edit_conf "bitcoind.zmqpubrawblock" ${BITCOIND_ZMQRAWBLOCK}
-  fi
+    if [ ! -z "${BITCOIND_ZMQRAWBLOCK}" ] && [ -f "$LND_CONF_PATH" ]; then
+      edit_conf "bitcoind.zmqpubrawblock" ${BITCOIND_ZMQRAWBLOCK}
+    fi
 
-  if [ ! -z "${BITCOIND_ZMQRAWTX}" ] && [ -f "$LND_CONF_PATH" ]; then
-    edit_conf "bitcoind.zmqpubrawtx" ${BITCOIND_ZMQRAWTX}
-  fi
+    if [ ! -z "${BITCOIND_ZMQRAWTX}" ] && [ -f "$LND_CONF_PATH" ]; then
+      edit_conf "bitcoind.zmqpubrawtx" ${BITCOIND_ZMQRAWTX}
+    fi
 
+    exec su-exec lnd lnd
+  else
+    cmd="$*"
+    if [ ! -z "${BITCOIND_RPCPASS}" ]; then
+      echo "Replacing bitcoind rpc password"
+      cmd=${cmd/BITCOIND_RPCPASS/$BITCOIND_RPCPASS}
+    fi
+
+    exec su-exec lnd $cmd
+  fi
+elif [ "$1" = "lnd-cli" ] || [ "$1" = "lnd-tx" ]; then
   exec su-exec lnd "$@"
+else
+  echo "Executing $@"
+  exec "$@"
 fi
-
-echo "Executing $@"
-exec "$@"
