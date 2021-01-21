ab9a16c14145ad1a3dd2fd89ef5a48a1e689719e
Victor Afanassieff <victor@afanassieff.com>
2021-01-21 16:16:10 +0100

    Added lnd-channels-backup

--- lnd-backup-channels/build.sh
+++ lnd-backup-channels/build.sh
@@ -2,25 +2,34 @@
 
 set -e
 
-NAME="lnd-backup-channels"
+cd docker 
+
+# For docker hub ...
+IMAGE_NAME="vafanassieff/lnd-channels-backup"
+docker build -t $IMAGE_NAME .
+echo "$IMAGE_NAME built √"
 
 if [ "$1" == "registry" ]; then
-    IMAGE="vafanassieff/$NAME"
-    echo "Building $IMAGE"
-    docker build -t $IMAGE:latest .
-    docker login registry.gitlab.com
-    docker push $IMAGE:latest
-    echo "$IMAGE:latest has been built √"
+    docker push $IMAGE_NAME
+    echo "$IMAGE_NAME pushed √"
+    if [ "$2" == "latest" ]; then
+        echo "Add the latest tag to the image"
+        docker tag  $IMAGE_NAME $IMAGE_NAME:latest
+        docker push $IMAGE_NAME:latest
+        echo "$IMAGE_NAME:latest pushed √"
+    fi
+fi
 
-    # For my personal registry ...
-    IMAGE="registry.afa.ovh/$NAME"
-    echo "Building $IMAGE"
-    docker build -t $IMAGE:latest .
-    docker login registry.gitlab.com
-    docker push $IMAGE:latest
-    echo "$IMAGE:latest has been built √"
-else
-    echo "Building $IMAGE:dev"
-    docker build -t $IMAGE:dev .
-    echo "$IMAGE:dev has been built √"
+# For my personal registry ...
+IMAGE_NAME="registry.afa.ovh/lnd-channels-backup"
+docker build -t $IMAGE_NAME .
+if [ "$1" == "registry" ]; then
+    docker push $IMAGE_NAME
+    echo "$IMAGE_NAME pushed √"
+    if [ "$2" == "latest" ]; then
+        echo "Add the latest tag to the image"
+        docker tag  $IMAGE_NAME $IMAGE_NAME:latest
+        docker push $IMAGE_NAME:latest
+        echo "$IMAGE_NAME:latest pushed √"
+    fi
 fi
--- lnd-backup-channels/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-set -e
-
-echo "Executing $@"
-exec "$@"
