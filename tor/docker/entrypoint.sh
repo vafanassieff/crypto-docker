9cf32dbc4700d87b93e8ea541eb4430638068a69
Victor Afanassieff <victor@afanassieff.com>
2021-01-21 15:42:16 +0100

    Fixed tor

--- tor/docker/entrypoint.sh
+++ tor/docker/entrypoint.sh
@@ -2,34 +2,32 @@
 
 set -e
 
-TOR_CONF_PATH=/etc/tor/torrc
-
-if [ ! -z "$UID" ]; then
-    usermod -u $UID tor
+if [ ! -z "$USER_ID" ]; then
+    usermod -u $USER_ID tor
 fi
 
-if [ ! -z "$GID" ]; then
-    groupmod -g $GID tor
+if [ ! -z "$GROUP_ID" ]; then
+    groupmod -g $GROUP_ID tor
 fi
-
-rm -f $TOR_CONF_PATH
-
-if [ ! -z "$PASSWORD" ]; then
-    echo HashedControlPassword $(tor --hash-password "${PASSWORD}" | tail -n 1) >> /etc/tor/torrc
-fi
-
-for tor_options in $(printenv | grep TOR_)
-do
-    options="${tor_options:4}"
-    readarray -d = -t options <<< "$options"
-    option=${options[0]}
-    value=${options[1]}
-    echo $option $value >> $TOR_CONF_PATH
-done
 
 if [ $1 = "tor" ];then
-    su-exec tor tor -f $TOR_CONF_PATH --verify-config
-    exec su-exec tor tor -f $TOR_CONF_PATH
+    rm -f $CONF_PATH
+
+    if [ ! -z "$PASSWORD" ]; then
+        echo HashedControlPassword $(tor --hash-password "${PASSWORD}" | tail -n 1) >> /etc/tor/torrc
+    fi
+
+    for tor_options in $(printenv | grep TOR_)
+    do
+        options="${tor_options:4}"
+        readarray -d = -t options <<< "$options"
+        option=${options[0]}
+        value=${options[1]}
+        echo $option $value >> $CONF_PATH
+    done
+    cat /etc/tor/torrc
+    su-exec tor tor -f $CONF_PATH --verify-config
+    exec su-exec tor tor -f $CONF_PATH
 else
     exec "$@"
 fi
