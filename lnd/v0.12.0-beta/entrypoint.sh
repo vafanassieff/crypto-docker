4846194da114242665757ccce0dd73117dca0fd1
Victor Afanassieff <victor@afanassieff.com>
2021-02-12 14:38:39 +0100

    Fixed entrypoint

--- lnd/v0.12.0-beta/entrypoint.sh
+++ lnd/v0.12.0-beta/entrypoint.sh
@@ -33,13 +33,14 @@ else
   echo "/lnd user and group is good !"
 fi
 
-if [ ! -z "${BITCOIND_RPCPASS}" ] && [ -f $LND_CONF_PATH ] && [ grep -q BITCOIND_RPCPASS $LND_CONF_PATH]; then
+if [ ! -z "${BITCOIND_RPCPASS}" ] && [ -f $LND_CONF_PATH ] && grep -q BITCOIND_RPCPASS $LND_CONF_PATH; then
   sed "s/BITCOIND_RPCPASS/$BITCOIND_RPCPASS/g" $LND_CONF_PATH > tmp.conf
   cat tmp.conf > $LND_CONF_PATH
   rm tmp.conf
 fi
-if [ ! -z "${TOR_PASSWORD}" ] && [ -f $LND_CONF_PATH ] && [ grep -q TOR_PASSWORD $LND_CONF_PATH]; then
-  sed "s/BITCOIND_RPCPASS/$TOR_PASSWORD/g" $LND_CONF_PATH > tmp.conf
+
+if [ ! -z "${TOR_PASSWORD}" ] && [ -f $LND_CONF_PATH ] && grep -q TOR_PASSWORD $LND_CONF_PATH; then
+  sed "s/TOR_PASSWORD/$TOR_PASSWORD/g" $LND_CONF_PATH > tmp.conf
   cat tmp.conf > $LND_CONF_PATH
   rm tmp.conf
 fi
